32f6fad820eaaa4df4a0572877cd374dc995c82d
kalyan63 <57671470+kalyan63@users.noreply.github.com>
2021-01-31 01:53:31 +0530

    Update q6_Bagging.py

--- assignment-1/q6_Bagging.py
+++ assignment-1/q6_Bagging.py
@@ -13,8 +13,8 @@ from metrics import *
 from ensemble.bagging import BaggingClassifier
 from tree.base import DecisionTree
 # Or use sklearn decision tree
-from linearRegression.linearRegression import LinearRegression
-
+# from linearRegression.linearRegression import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
 ########### BaggingClassifier ###################
 
 N = 30
@@ -25,13 +25,13 @@ X = pd.DataFrame(np.abs(np.random.randn(N, P)))
 y = pd.Series(np.random.randint(NUM_OP_CLASSES, size = N), dtype="category")
 
 criteria = 'information_gain'
-tree = DecisionTree(criterion=criteria)
+tree = DecisionTreeClassifier()
 Classifier_B = BaggingClassifier(base_estimator=tree, n_estimators=n_estimators )
 Classifier_B.fit(X, y)
 y_hat = Classifier_B.predict(X)
-[fig1, fig2] = Classifier_B.plot()
+Classifier_B.plot(X)
 print('Criteria :', criteria)
 print('Accuracy: ', accuracy(y_hat, y))
-for cls in y.unique():
-    print('Precision: ', precision(y_hat, y, cls))
-    print('Recall: ', recall(y_hat, y, cls))
+for cls in np.unique(y):
+    print('Precision of {} is: '.format(cls), precision(y_hat, y, cls))
+    print('Recall of {} is: '.format(cls), recall(y_hat, y, cls))
