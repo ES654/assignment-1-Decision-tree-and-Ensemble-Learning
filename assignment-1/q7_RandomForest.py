8ed7e9670ecad564df000598997fb7b639cb276b
kalyan63 <57671470+kalyan63@users.noreply.github.com>
2021-01-31 12:27:15 +0530

    Update q7_RandomForest.py

--- assignment-1/q7_RandomForest.py
+++ assignment-1/q7_RandomForest.py
@@ -1,138 +1,47 @@
-from numpy.core.fromnumeric import shape
-from .base import DecisionTree
-import matplotlib.pyplot as plt
-from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.tree import DecisionTreeRegressor
+"""
+The current code given is for the Assignment 2.
+> Classification
+> Regression
+"""
+
 import numpy as np
 import pandas as pd
-class RandomForestClassifier():
-    def __init__(self, n_estimators=100, criterion='gini', max_depth=None):
-        '''
-        :param estimators: DecisionTree
-        :param n_estimators: The number of trees in the forest.
-        :param criterion: The function to measure the quality of a split.
-        :param max_depth: The maximum depth of the tree.
-        '''
-        self.max_depth=max_depth
-        self.n_estimators=n_estimators
-        self.Forest=[None]*n_estimators
+import matplotlib.pyplot as plt
 
-    def fit(self, X, y):
-        """
-        Function to train and construct the RandomForestClassifier
-        Inputs:
-        X: pd.DataFrame with rows as samples and columns as features (shape of X is N X P) where N is the number of samples and P is the number of columns.
-        y: pd.Series with rows corresponding to output variable (shape of Y is N)
-        """
-        X_temp1=X.copy()
-        X_temp1["res"]=y
-        for i in range(self.n_estimators):
-            X_temp=X_temp1.sample(frac=0.6)
-            Dt=DecisionTreeClassifier(max_features=1)
-            Dt.fit(X_temp.iloc[:,:-1],X_temp.iloc[:,-1])
-            self.Forest[i]=Dt
+from metrics import *
 
-    def predict(self, X):
-        """
-        Funtion to run the RandomForestClassifier on a data point
-        Input:
-        X: pd.DataFrame with rows as samples and columns as features
-        Output:
-        y: pd.Series with rows corresponding to output variable. THe output variable in a row is the prediction for sample in corresponding row in X.
-        """
-        res=np.zeros((X.shape[0],self.n_estimators))
-        print(res)
-        for i in range(self.n_estimators):
-            Dt=self.Forest[i]
-            res[:,i]=np.array(Dt.predict(X))
-        y_hat=np.zeros(X.shape[0])
-        for i in range(X.shape[0]):
-            a=list(res[i])
-            y_hat[i]=max(set(a),key=a.count)
-        return pd.Series(y_hat)
+from tree.randomForest import RandomForestClassifier
+from tree.randomForest import RandomForestRegressor
 
-    def plot(self,X):
-        """
-        Function to plot for the RandomForestClassifier.
-        It creates three figures
+np.random.seed(42)
 
-        1. Creates a figure with 1 row and `n_estimators` columns. Each column plots the learnt tree. If using your sklearn, this could a matplotlib figure.
-        If using your own implementation, it could simply invole print functionality of the DecisionTree you implemented in assignment 1 and need not be a figure.
+########### RandomForestClassifier ###################
 
-        2. Creates a figure showing the decision surface for each estimator
+N = 30
+P = 5
+X = pd.DataFrame(np.random.randn(N, P))
+y = pd.Series(np.random.randint(P, size = N), dtype="category")
+for criteria in ['information_gain', 'gini_index']:
+    Classifier_RF = RandomForestClassifier(10, criterion = criteria)
+    Classifier_RF.fit(X, y)
+    y_hat = Classifier_RF.predict(X)
+    Classifier_RF.plot()
+    print('Criteria :', criteria)
+    print('Accuracy: ', accuracy(y_hat, y))
+    for cls in y.unique():
+        print('Precision of {} is: '.format(cls), precision(y_hat, y, cls))
+        print('Recall of {} is: '.format(cls), recall(y_hat, y, cls))
 
-        3. Creates a figure showing the combined decision surface
+########### RandomForestRegressor ###################
 
-        """
-        for i in range(self.n_estimators):
-            tree.plot_tree(self.Forest[i])
-            temp="Tree number"+str(i)
-            plt.title(temp)
-            plt.show()
+N = 30
+P = 5
+X = pd.DataFrame(np.random.randn(N, P))
+y = pd.Series(np.random.randn(N))
 
-
-
-
-
-class RandomForestRegressor():
-    def __init__(self, n_estimators=100, criterion='variance', max_depth=None):
-        '''
-        :param n_estimators: The number of trees in the forest.
-        :param criterion: The function to measure the quality of a split.
-        :param max_depth: The maximum depth of the tree.
-        '''
-        self.n_estimators=n_estimators
-        self.Forest=[None]*n_estimators
-
-    def fit(self, X, y):
-        """
-        Function to train and construct the RandomForestRegressor
-        Inputs:
-        X: pd.DataFrame with rows as samples and columns as features (shape of X is N X P) where N is the number of samples and P is the number of columns.
-        y: pd.Series with rows corresponding to output variable (shape of Y is N)
-        """
-        X_temp1=X.copy()
-        X_temp1["res"]=y
-        for i in range(self.n_estimators):
-            X_temp=X_temp1.sample(frac=0.6)
-            Dt=DecisionTreeRegressor(max_features=1)
-            Dt.fit(X_temp.iloc[:,:-1],X_temp.iloc[:,-1])
-            self.Forest[i]=Dt
-
-    def predict(self, X):
-        """
-        Funtion to run the RandomForestRegressor on a data point
-        Input:
-        X: pd.DataFrame with rows as samples and columns as features
-        Output:
-        y: pd.Series with rows corresponding to output variable. THe output variable in a row is the prediction for sample in corresponding row in X.
-        """
-        res=np.zeros((X.shape[0],self.n_estimators))
-        for i in range(self.n_estimators):
-            Dt=self.Forest[i]
-            res[:,i]=np.array(Dt.predict(X))
-        y_hat=np.zeros(X.shape[0])
-        for i in range(X.shape[0]):
-            y_hat[i]=np.mean(res[i])
-        return pd.Series(y_hat)
-
-    def plot(self):
-        """
-        Function to plot for the RandomForestClassifier.
-        It creates three figures
-
-        1. Creates a figure with 1 row and `n_estimators` columns. Each column plots the learnt tree. If using your sklearn, this could a matplotlib figure.
-        If using your own implementation, it could simply invole print functionality of the DecisionTree you implemented in assignment 1 and need not be a figure.
-
-        2. Creates a figure showing the decision surface/estimation for each estimator. Similar to slide 9, lecture 4
-
-        3. Creates a figure showing the combined decision surface/prediction
-
-        """
-        for i in range(self.n_estimators):
-            tree.plot_tree(self.Forest[i])
-            temp="Tree number"+str(i)
-            plt.title(temp)
-            plt.show()
-
+Regressor_RF = RandomForestRegressor(10)
+Regressor_RF.fit(X, y)
+y_hat = Regressor_RF.predict(X)
+Regressor_RF.plot()
+print('RMSE: ', rmse(y_hat, y))
+print('MAE: ', mae(y_hat, y))
